9fda2183cbaf2b610b48793bf897893a5626ba20
SimonMai6 <11643406+SimonMai6@users.noreply.github.com>
2024-04-29 16:56:57 -0700

    Pass third test
    Different from the solution but still passed all
    the tests. Need to remember next time to think
    about what methods I can possibly use to make
    the code less complex.
    
    Solution used split('') to make the string into
    an array and then using reverse() to reverse the
    array and join('') to convert it back into a
    string.
    
    The solution that I came up with was using a for
    loop that loops through the length of the array
    by decrementing down. Created a string outside
    of the loop and added that string with the given
    string with method charAt().

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    reverse = '';
+    for(let i = words.length; 0 <= i; i--){
+        reverse += words.charAt(i);
+    }
+    return reverse;
 };
 
 // Do not edit below this line
