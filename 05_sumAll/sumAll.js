177fccabe28cb8e362f45a669cd6ea95687c8d41
SimonMai6 <11643406+SimonMai6@users.noreply.github.com>
2024-05-03 16:53:52 -0700

    Function sum all
    Sum all function takes  two integers and
    adds all numbers in between the two integers and
    the integers themselves.
    
    If statements returns error if an non-integer was
    given or if it is an negative number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sumOfNums = 0;
+    if(firstNum < 0 || secondNum < 0 
+        || typeof firstNum !== 'number'
+        || typeof secondNum !== 'number'){
+        return 'ERROR';
+    }
+    while(firstNum > secondNum){
+        sumOfNums += secondNum;
+        secondNum++;
+    }
+
+    while(firstNum <= secondNum){
+        sumOfNums += firstNum;
+        firstNum++;
+    }
+    return sumOfNums;
 
 };
 
