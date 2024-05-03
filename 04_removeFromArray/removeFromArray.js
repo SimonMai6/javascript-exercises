23157e8c7a68ce14d461deb48a00ac6753292408
SimonMai6 <11643406+SimonMai6@users.noreply.github.com>
2024-05-03 16:02:37 -0700

    Passed test three
    Created a different way of implementing the
    function compared to the solution. Used a nested
    loop, first using a for loop and then used a while
    loop.
    
    Was not sure how the function's parameters
    worked when given more than what the parameters
    asked for. However, I now know that if the
    arguments are more than the parameters you set,
    then it will make an array like object. Sort of
    like a dictionary in python.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    newArray = [];
+    
+    // for(let i = 1; i < arguments.length; i++){
+    //     while(array.includes(arguments[i])){
+    //         array.splice(array.indexOf(arguments[i]),1);
+    //     }
+        
+    // }
+    for(const element of array){
+        if(!args.includes(element)){
+            newArray.push(element);
+        }
+    }
+    
+    return newArray;
 };
 
 // Do not edit below this line
