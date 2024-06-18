684f900e7b77922819b56781202b202f12c0b334
SimonMai6 <11643406+SimonMai6@users.noreply.github.com>
2024-06-17 17:06:30 -0700

    Finished eighth assignment
    Used a different solution for the factorial.
    Created an array that has an index of the number
    that is given and used the keys() method that
    returned the array's numerable string-keyed
    property names.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
+const add = function(numOne, numTwo) {
+  let additionSum = numOne + numTwo;
+  return additionSum;
 	
 };
 
-const subtract = function() {
+const subtract = function(numOne, numTwo) {
+	let subtractSum = numOne - numTwo;
+  return subtractSum;
+};
+
+const sum = function(arr) {
+  let sum = arr.reduce((total, currentNum) => total + currentNum, 0);
+  return sum;
 	
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let multiplySum = arr.reduce((total, currentNum) => total * currentNum, 1);
+  return multiplySum;
 };
 
-const multiply = function() {
-
+const power = function(numOne, numTwo) {
+	let powerSum = Math.pow(numOne,numTwo);
+  return powerSum
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  const arr = ([...Array(num+1).keys()]);
+  arr.shift();
+  let factorialSum = arr.reduce((total, currentNum) => total * currentNum, 1);
+  return factorialSum;
 };
 
 // Do not edit below this line
