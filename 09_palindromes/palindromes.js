2b1c1957d9902fb65c019f1badb8297029c1d65e
SimonMai6 <11643406+SimonMai6@users.noreply.github.com>
2024-06-21 16:06:04 -0700

    Completed assignment 08
    Comparing to the given solution, my solution used
    replace to remove all of the punctuations and
    word breaks. Probably should have used filter()
    method for practice, but found the regular
    expression 'g' modifier that was used to remove
    all punctuations and word breaks.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    const arr = palindrome.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "").split("");
+    let notReversed = arr.join("").toLowerCase();
+    let reverse = arr.reverse().join("").toLowerCase();
+    return notReversed.includes(reverse);
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
