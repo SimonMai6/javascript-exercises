e544fe12d614f22ba879688fff93b8d59149aec4
SimonMai6 <11643406+SimonMai6@users.noreply.github.com>
2024-06-23 16:25:05 -0700

    Complete assignment 10
    Went the route of making an array with [0,1] to
    make it able to continue the fibonacci
    sequence. It is a bit similar to the solution that
    was given.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num >= 0 ){
+        const fibonacciSequence = [0,1];
+        for(let i = 0; num-1 > i; i++){
+            fibonacciSequence.push(fibonacciSequence[i] + fibonacciSequence[i+1]);  
+        }
+        return fibonacciSequence[num];
+    }
+    else{
+        return "OOPS";
+    }
+    
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
