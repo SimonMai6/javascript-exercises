c8b25c7fe54c85b18f29ab778305bee057d62008
SimonMai6 <11643406+SimonMai6@users.noreply.github.com>
2024-06-23 17:16:26 -0700

    Finished assignment 12
    Used if else statements to see if whether
    there were or were not death dates. Compared
    to the solutions folder, I could have made a
    separate function to check for that.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear().toString();
+    let somePerson = people.reduce((a, b) => {
+        if(isNaN(a.yearOfDeath)){
+            a.yearOfDeath = currentYear;
+        }
+        else if(isNaN(b.yearOfDeath)){
+            b.yearOfDeath = currentYear;
+        }
+        if(a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth){
+            return a;
+        }
+        else{
+            return b;
+        } 
+       
+        
+    });
+    return somePerson;
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
